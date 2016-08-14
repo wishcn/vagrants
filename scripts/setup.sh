6e2643a0f3d9301a4b42469227c5ede154e9f13e
审判天使 <xxstop@qq.com>
2016-08-14 22:45:13 +0800

    Add tuna sources

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,14 +7,21 @@ sudo sed -i 's/archive.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 # update
 sudo apt-get -y update && apt-get -y upgrade
 
-# wget curl
-sudo apt-get install -y wget curl git
-
 # docker
-# sudo apt-get install -y docker.io
+sudo apt-get -y install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' > /etc/apt/sources.list.d/docker.list
+sudo apt-get -y update
+sudo apt-get -y install docker-engine
+## docker not sudo
+sudo groupadd docker
+sudo gpasswd -a ${USER} docker
+
+# wget curl
+sudo apt-get -y install wget curl git
 
 # tmux zsh
-sudo apt-get install -y tmux zsh
+sudo apt-get -y install tmux zsh
 
 # tmux config
 wget -O ~/.tmux.conf https://raw.githubusercontent.com/xxstop/vagrants/ubuntu4win/.tmux.conf
@@ -24,4 +31,4 @@ sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/to
 sudo sed -i "s/\/home\/vagrant:\/bin\/bash/\/home\/vagrant:\/bin\/zsh/g" /etc/passwd
 
 # emacs vim
-sudo apt-get install -y vim emacs
+sudo apt-get -y install vim emacs
