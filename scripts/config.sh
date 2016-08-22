8b5714a114d4426c2ae793f0ac527e0b2808346b
qiuwenxi <qiuwenxi@zbj.com>
2016-08-22 15:59:08 +0800

    Add .

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-git config --global user.name "xxstop" && git config --global user.email "xxstop@qq.com"
+git config --global user.name "xxstop" && git config --global user.email "xxstop@qq.com" \
+    && git config --global push.default simple
